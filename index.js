fa87b616c8f95d9475d07696365ee9a948bbd77f
code-with-ayesha <139987976+code-with-ayesha@users.noreply.github.com>
2023-09-15 20:04:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// let myname = "Ayesha Ijaz";
+// let todayDate = 29-7-2023;
+// output will be in four lines with the number 8.
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
